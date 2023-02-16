4810d4681820d192774c92867aad74cba36ce055
Aleksei <jojmusor@gmail.com>
2023-02-17 01:18:52 +0300

    refactoring and adding error handlers

--- src/routes/getCurrency.ts
+++ src/routes/getCurrency.ts
@@ -1,18 +1,24 @@
 import { Router } from 'express';
 import { Request, Response } from 'express';
 import axios from 'axios'
-import { config } from '../config/config';
+import PG from '../db';
 
 
 export const getCurrency = Router()
 
 getCurrency.post('/getCurrency', (req: Request, res: Response) => {
-	// https://api.getgeoapi.com/v2/currency/convert?api_key=${proccess.env.API_KEY}&from=EUR&to=GBP&amount=1&format=json
-	req.body.from
-	req.body.to
-	// Need add validation!!!
-	axios({ url: `https://api.getgeoapi.com/v2/currency/convert?api_key=${process.env.API_KEY}&from=${req.body.from}&to=${req.body.to}&amount=1&format=json` })
+	axios({
+		url:
+			`https://api.getgeoapi.com/v2/currency/convert?api_key=${process.env.THIRD_APP_API_KEY}&from=${req.body.from}&to=${req.body.to}&amount=1&format=json`
+	})
 		.then(val => {
+			const pg = new PG()
+			pg.query(
+				`INSERT INTO currency_log VALUES (${new Date()}, ${req.body.from}, ${req.body.to});`)
 			res.json(val.data)
+		}, err => {
+			res.status(404)
+			res.send(err)
 		})
+	// NEED ADD LOGGING OF SUCCESS REQUEST TO DB (POSTGRESQL)
 })
--- src/routes/getCurrencyByDate.ts
+++ src/routes/getCurrencyByDate.ts
@@ -1,12 +1,33 @@
 import { Router } from 'express';
 import { Request, Response } from 'express';
 import axios from 'axios';
-
+import PG from '../db';
+import { config } from 'dotenv';
+config()
 
 export const getCurrencyByDate = Router()
 
-getCurrencyByDate.get('/getCurrencyByDate', (req: Request, res: Response) => {
-
-
-	// https://api.getgeoapi.com/v2/currency/historical/2018-10-12?api_key=${proccess.env.API_KEY}&from=EUR&to=GBP&amount=1&format=json
+getCurrencyByDate.post('/getCurrencyByDate', (req: Request, res: Response) => {
+	// date format - YYYY-MM-DD 2018-10-12
+	axios({
+		url:
+			`https://api.getgeoapi.com/v2/currency/historical/${req.body.date}?api_key=${process.env.THIRD_APP_API_KEY}&from=${req.body.from}&to=${req.body.to}&amount=1&format=json`
+	})
+		.then(async val => {
+			const pg = new PG()
+			await pg.query(
+				`INSERT INTO currency_log VALUES (${new Date()}, ${req.body.from}, ${req.body.to});`)
+				.then((dbResult) => {
+					console.log('db res: ' + dbResult)
+					res.json(val.data)
+				})
+				.catch(reason => {
+					res.status(408)
+					res.send(reason)
+				})
+		}, err => {
+			res.status(404)
+			res.send(err)
+		})
+	// NEED ADD LOGGING OF SUCCESS REQUEST TO DB (POSTGRESQL)
 })
